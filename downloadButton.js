9cc3ed236c4803cdbfb1e7324d66afd20383e4eb
PalmerAL <PalmerAL@users.noreply.github.com>
2021-06-19 17:50:00 -0500

    Update links for 1.20

--- downloadButton.js
+++ downloadButton.js
@@ -99,14 +99,14 @@ var failMessage = "Min isn't supported on your OS"
 
 var downloadLinks = {
   mac: {
-    intel: 'https://github.com/minbrowser/min/releases/download/v1.19.2/Min-v1.19.2-darwin-x64.zip',
-    arm: 'https://github.com/minbrowser/min/releases/download/v1.19.2/Min-v1.19.2-darwin-arm64.zip'
+    intel: 'https://github.com/minbrowser/min/releases/download/v1.20.0/Min-v1.20.0-darwin-x64.zip',
+    arm: 'https://github.com/minbrowser/min/releases/download/v1.20.0/Min-v1.20.0-darwin-arm64.zip'
   },
   linux: {
-    deb: 'https://github.com/minbrowser/min/releases/download/v1.19.2/min_1.19.2_amd64.deb',
-    rpm: 'https://github.com/minbrowser/min/releases/download/v1.19.2/min-1.19.2-1.x86_64.rpm'
+    deb: 'https://github.com/minbrowser/min/releases/download/v1.20.0/min_1.20.0_amd64.deb',
+    rpm: 'https://github.com/minbrowser/min/releases/download/v1.20.0/min-1.20.0-1.x86_64.rpm'
   },
-  windows: 'https://github.com/minbrowser/min/releases/download/v1.19.2/min-1.19.2-setup.exe'
+  windows: 'https://github.com/minbrowser/min/releases/download/v1.20.0/min-1.20.0-setup.exe'
 }
 
 function getUserPlatform () {
